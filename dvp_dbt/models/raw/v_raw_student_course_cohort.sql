d6f31374e3b2cc14e985b81e6ab249654954fc18
slava golyshevskii <sgolyshevskii@gmail.com>
2024-01-06 10:31:23 +0400

    Update: Added student satellite data (#5)
    * Refactor script
    
    * Added student satellite data

--- dvp_dbt/models/raw/v_raw_student_course_cohort.sql
+++ dvp_dbt/models/raw/v_raw_student_course_cohort.sql
@@ -3,6 +3,7 @@ select
     upper(trim(scc.course_cohort)) as cohort_bk,
     scc.student_id as student_bk,
     lower(trim(scc.student_email)) as student_email,
+    lower(trim(scc.student_username)) as student_username,
     initcap(trim(scc.student_name)) as student_name,
     scc.enrollment_dt,
     scc.load_dt
